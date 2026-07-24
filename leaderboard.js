bd09e3753813bc15fdf6a1a92d751b4254a912b2
agungpratikto13-dotcom <agungpratikto13@gmail.com>
2026-07-25 04:39:52 +0700

    Create leaderboard.js

---
+++ leaderboard.js
@@ -0,0 +1,72 @@
+let dataScore =
+JSON.parse(localStorage.getItem("score")) || [];
+
+
+function simpanScore(nama,kpm){
+
+
+dataScore.push({
+
+nama:nama,
+kpm:kpm
+
+});
+
+
+dataScore.sort(
+(a,b)=>b.kpm-a.kpm
+);
+
+
+dataScore=dataScore.slice(0,10);
+
+
+
+localStorage.setItem(
+"score",
+JSON.stringify(dataScore)
+);
+
+
+tampilLeaderboard();
+
+
+}
+
+
+
+function tampilLeaderboard(){
+
+
+let box=document.getElementById("leaderboard");
+
+
+box.innerHTML="";
+
+
+dataScore.forEach((pemain,index)=>{
+
+
+box.innerHTML+=`
+
+<div>
+
+<b>${index+1}. ${pemain.nama}</b>
+
+<br>
+
+⚡ ${pemain.kpm} KPM
+
+</div>
+
+<hr>
+
+`;
+
+});
+
+
+}
+
+
+tampilLeaderboard();
