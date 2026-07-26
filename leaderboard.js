4ddcd84f2f10e6e03cd9fd21ba7c34b59e41aab8
agungpratikto13-dotcom <agungpratikto13@gmail.com>
2026-07-27 05:37:32 +0700

    Update leaderboard.js

--- leaderboard.js
+++ leaderboard.js
@@ -31,18 +31,29 @@ export function tampilLeaderboard() {
       return;
     }
 
-    snapshot.forEach((docSnap, index) => {
-      const pemain = docSnap.data();
+  snapshot.forEach((docSnap, index) => {
 
-      box.innerHTML += `
-        <div>
-          <b>${index + 1}. ${pemain.nama}</b>
-          <br>
-          ⚡ ${pemain.kpm} KPM
+    const pemain = docSnap.data();
+
+    let kelas = "";
+
+    if(index === 0){
+        kelas = "rank1";
+    }else if(index === 1){
+        kelas = "rank2";
+    }else if(index === 2){
+        kelas = "rank3";
+    }
+
+    box.innerHTML += `
+        <div class="${kelas}">
+            <b>${index + 1}. ${pemain.nama}</b>
+            <br>
+            ⚡ ${pemain.kpm} KPM
         </div>
-        <hr>
-      `;
-    });
+    `;
+
+});
   });
 }
 
@@ -87,3 +98,71 @@ export async function simpanScore(nama, kpm) {
 
 // Jalankan leaderboard saat halaman dibuka
 tampilLeaderboard();
+
+.rank1{
+
+    background:linear-gradient(145deg,#FFD700,#FFC107);
+
+    color:#222;
+
+    padding:18px;
+
+    border-radius:18px;
+
+    margin-bottom:15px;
+
+    font-size:22px;
+
+    font-weight:700;
+
+    box-shadow:
+        0 15px 35px rgba(255,215,0,.6),
+        inset 0 2px 5px rgba(255,255,255,.8);
+
+    transform:perspective(800px) rotateX(8deg);
+
+    transition:.3s;
+
+}
+
+.rank1:hover{
+
+    transform:perspective(800px) rotateX(0deg) scale(1.04);
+
+}
+
+.rank2{
+
+    background:linear-gradient(145deg,#d9d9d9,#a7a7a7);
+
+    color:#222;
+
+    padding:16px;
+
+    border-radius:15px;
+
+    margin-bottom:12px;
+
+    font-weight:700;
+
+    box-shadow:0 10px 25px rgba(255,255,255,.3);
+
+}
+
+.rank3{
+
+    background:linear-gradient(145deg,#cd7f32,#a0522d);
+
+    color:white;
+
+    padding:16px;
+
+    border-radius:15px;
+
+    margin-bottom:12px;
+
+    font-weight:700;
+
+    box-shadow:0 10px 25px rgba(205,127,50,.4);
+
+}
